d837f17b575de4146c310349caa8dbeef9fc2dbd
Julia <you@example.com>
2021-07-21 10:39:15 +0200

    The first commit of mynewplugin

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Julia"
 plugin_author_email = "kouchoelojulia@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/you/OctoPrint-MyNewPlugin"
+plugin_url = "https://github.com/Julia-66h/OctoPrint-MyNewPlugin"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
